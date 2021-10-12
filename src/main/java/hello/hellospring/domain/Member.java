e32ed8d4a51fb9a43cde8c34120206a6bbac086f
mina-gwak <mina.gwak@kakao.com>
2021-10-12 16:55:33 +0900

    JPA

--- src/main/java/hello/hellospring/domain/Member.java
+++ src/main/java/hello/hellospring/domain/Member.java
@@ -1,9 +1,15 @@
 package hello.hellospring.domain;
 
+import javax.persistence.*;
+
+@Entity
 public class Member {
 
     // ID : 고객이 정하는 것이 아니라 시스템이 정하는 임의의 값
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+//    @Column(name = "username")
     private String name;
 
     public Long getId() {
